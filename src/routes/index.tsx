f9a1e6cdc9369c3a75a115c2e0f5eba3945eb85c
notmaiku <mike1davis@rocketmail.com>
2024-03-19 19:47:04 -0400

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { Title } from "@solidjs/meta";
+import { createAsync } from "@solidjs/router";
+import Counter from "~/components/Counter";
+import { api } from "~/lib/api";
+
+export default function Home() {
+  const hello = createAsync(() => api.example.hello.query("world"));
+  const start = createAsync(() => api.example.start.query());
+  return (
+    <main>
+      <Title>Hello World</Title>
+      <h1>Hello world!</h1>
+      <Counter />
+      <p>
+        Visit{" "}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{" "}
+        to learn how to build SolidStart apps.
+      </p>
+      <pre>
+        <code>{JSON.stringify(hello(), null, 2)}</code>
+        <code>{JSON.stringify(start(), null, 2)}</code>
+      </pre>
+    </main>
+  );
+}
---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,16 @@
+import { wrap } from "@decs/typeschema";
+import { string } from "valibot";
+import { createTRPCRouter, publicProcedure } from "../utils";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(wrap(string()))
+    .query(({ input }) => {
+      return `Hello ${input}!`;
+    }),
+  start: publicProcedure
+    .query(() => {
+      return `this is trpc`;
+    })
+
+});
